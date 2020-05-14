30178f8e436968590de3aed1e32969c092449dd2
NickBehrends <halter456@gmail.com>
2020-05-14 07:57:16 -0500

    Added interface for bulk repository

---
+++ src/CSESoftware.Repository/IBulkRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CSESoftware.Core.Entity;
+
+namespace CSESoftware.Repository
+{
+    public interface IBulkRepository
+    {
+        void Create<TEntity>(IList<TEntity> data)
+            where TEntity : class, IBaseEntity;
+    }
+}

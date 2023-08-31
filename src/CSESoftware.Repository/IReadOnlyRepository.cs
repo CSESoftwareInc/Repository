bd87d6abc77b2b07b0ff470dc197096da0db870c
Cameron Anderson <canderson3.14@me.com>
2023-08-31 16:36:50 -0500

    Update for synchronous calls

--- src/CSESoftware.Repository/IReadOnlyRepository.cs
+++ src/CSESoftware.Repository/IReadOnlyRepository.cs
@@ -1,23 +1,34 @@
-﻿using System;
+﻿using CSESoftware.Core.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
-using CSESoftware.Core.Entity;
 
 namespace CSESoftware.Repository
 {
     public interface IReadOnlyRepository {
-
         Task<List<TEntity>> GetAllAsync<TEntity>(IQuery<TEntity> filter)
+            where TEntity : class, IEntity;
+
+        List<TEntity> GetAll<TEntity>(IQuery<TEntity> filter)
             where TEntity : class, IEntity;
 
         Task<List<TEntity>> GetAllAsync<TEntity>(Expression<Func<TEntity, bool>> filter = null)
             where TEntity : class, IEntity;
 
+        List<TEntity> GetAll<TEntity>(Expression<Func<TEntity, bool>> filter = null)
+            where TEntity : class, IEntity;
+
         Task<TEntity> GetFirstAsync<TEntity>(IQuery<TEntity> filter)
             where TEntity : class, IEntity;
 
+        TEntity GetFirst<TEntity>(IQuery<TEntity> filter)
+            where TEntity : class, IEntity;
+
         Task<TEntity> GetFirstAsync<TEntity>(Expression<Func<TEntity, bool>> filter = null)
+            where TEntity : class, IEntity;
+
+        TEntity GetFirst<TEntity>(Expression<Func<TEntity, bool>> filter = null)
             where TEntity : class, IEntity;
 
         Task<int> GetCountAsync<TEntity>(IQuery<TEntity> filter)
@@ -35,7 +46,13 @@ namespace CSESoftware.Repository
         Task<List<TOut>> GetAllWithSelectAsync<TEntity, TOut>(IQueryWithSelect<TEntity, TOut> filter = null)
             where TEntity : class, IEntity;
 
+        List<TOut> GetAllWithSelect<TEntity, TOut>(IQueryWithSelect<TEntity, TOut> filter = null)
+            where TEntity : class, IEntity;
+
         Task<TOut> GetFirstWithSelectAsync<TEntity, TOut>(IQueryWithSelect<TEntity, TOut> filter = null)
+            where TEntity : class, IEntity;
+
+        TOut GetFirstWithSelect<TEntity, TOut>(IQueryWithSelect<TEntity, TOut> filter = null)
             where TEntity : class, IEntity;
     }
 }

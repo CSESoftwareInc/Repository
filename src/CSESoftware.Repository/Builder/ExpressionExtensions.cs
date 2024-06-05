ec06ee7795bc7ec30333ebe003f00722e6e5d807
Cameron Anderson <canderson3.14@me.com>
2024-06-05 15:28:08 -0500

    Add WhereAlso (#13)

---
+++ src/CSESoftware.Repository/Builder/ExpressionExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace CSESoftware.Repository.Builder
+{
+    internal static class ExpressionExtensions
+    {
+        internal static Expression<Func<T, bool>> AndAlso<T>(this Expression<Func<T, bool>> left, Expression<Func<T, bool>> right)
+        {
+            var parameter = Expression.Parameter(typeof(T), "entity");
+
+            var leftVisitor = new ReplaceExpressionVisitor(left.Parameters.First(), parameter);
+            var leftBody = leftVisitor.Visit(left.Body);
+
+            var rightVisitor = new ReplaceExpressionVisitor(right.Parameters.First(), parameter);
+            var rightBody = rightVisitor.Visit(right.Body);
+
+            var body = Expression.AndAlso(leftBody!, rightBody!);
+
+            return Expression.Lambda<Func<T, bool>>(body, parameter);
+        }
+
+        private class ReplaceExpressionVisitor(Expression oldValue, Expression newValue) : ExpressionVisitor
+        {
+            public override Expression Visit(Expression expression)
+            {
+                return expression == oldValue ? newValue : base.Visit(expression);
+            }
+        }
+    }
+}
